5be4a48e3adb8d4487a88ed7286e687c2d18e080
iVoidout <90503863+iVoidout@users.noreply.github.com>
2024-08-04 13:12:20 +0330

    cleaning up

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d E:\Programming\dnsChanger
-pyinstaller --noconfirm --onefile --windowed --icon "E:\Programming\dnsChanger\logo.ico" --name "Void DNS Shifter" --clean --noupx --hide-console "hide-early" "E:\Programming\dnsChanger\main.py" --distpath "E:\Programming\dnsChanger\output" --workpath "E:\Programming\dnsChanger\output\temp" --add-data "E:\Programming\dnsChanger\logo.ico;." --specpath "E:\Programming\dnsChanger\output\temp"
+pyinstaller --noconfirm --onefile --windowed --icon "E:\Programming\dnsChanger\logo.ico" --name "Void DNS Shifter" --clean --noupx --hide-console "hide-early" "E:\Programming\dnsChanger\main.py" --distpath "E:\Programming\voidDnsShifter\output" --workpath "E:\Programming\dnsChanger\output\temp" --add-data "E:\Programming\dnsChanger\logo.ico;." --specpath "E:\Programming\dnsChanger\output\temp"
 TIMEOUT 5
